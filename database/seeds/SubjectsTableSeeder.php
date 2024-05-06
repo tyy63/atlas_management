13761c794ac0153a747d43b40fa1218fc5e8709e
tyy63 <efb63t@gmail.com>
2024-05-06 16:30:57 +0900

    【初期データの追加】　対応完了 環境構築も併せて終了

--- database/seeds/SubjectsTableSeeder.php
+++ database/seeds/SubjectsTableSeeder.php
@@ -10,8 +10,15 @@ class SubjectsTableSeeder extends Seeder
      *
      * @return void
      */
+
     public function run()
     {
         // 国語、数学、英語を追加
+            DB::table('subjects')->insert
+        ([
+            ['subject' => '国語'],
+            ['subject' => '数学'],
+            ['subject' => '英語'],
+        ]);
     }
 }
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,6 +12,20 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-
+        DB::table('users') -> insert(
+        [
+            'over_name' =>'姓',
+            'under_name' =>'名',
+            'over_name_kana' =>'セイ',
+            'under_name_kana' =>'メイ',
+            'mail_address' =>'test21@gmail.com',
+            'sex' =>'1',
+            'birth_day' =>'2000-01-01',
+            'role' =>'1',
+            'password' => bcrypt ('testtest1'),
+            'created_at' =>'2000-01-01 00:00:01',
+            'updated_at' =>'2000-01-01 00:00:02',
+        ],
+    );
     }
 }
