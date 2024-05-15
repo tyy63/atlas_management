9dbd848a92f726ea009f8b3e160558361e3eff47
tyy63 <efb63t@gmail.com>
2024-05-15 21:55:39 +0900

    【予約している人数を表示】　対応　表示に関して修正し、再プッシュ

--- app/Calendars/Admin/CalendarWeekDay.php
+++ app/Calendars/Admin/CalendarWeekDay.php
@@ -32,13 +32,13 @@ class CalendarWeekDay{
     $html[] = '<div class="text-left">';
     if($one_part){
 // 予約席残数の表示
-      $html[] = '<p class="day_part m-0 pt-1">1部 ' . $one_part->limit_users . '</p>';
+      $html[] = '<p class="day_part m-0 pt-1">1部 ' . 20 -$one_part->limit_users . '</p>';
     }
     if($two_part){
-      $html[] = '<p class="day_part m-0 pt-1">2部 ' . $two_part->limit_users . '</p>';
+      $html[] = '<p class="day_part m-0 pt-1">2部 ' . 20 -$two_part->limit_users . '</p>';
     }
     if($three_part){
-      $html[] = '<p class="day_part m-0 pt-1">3部 ' . $three_part->limit_users . '</p>';
+      $html[] = '<p class="day_part m-0 pt-1">3部 ' . 20 -$three_part->limit_users . '</p>';
     }
     $html[] = '</div>';
 
